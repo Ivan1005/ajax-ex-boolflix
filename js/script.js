b39bf8f7da44361dd27cf7b5aff5b72c9adfd575
Ivan <matteraivam@gmail.com>
2020-09-02 18:02:06 +0200

    controllo film presente inserito

--- js/script.js
+++ js/script.js
@@ -10,26 +10,42 @@ $(document).ready(function() {
                     query: valore,
                     language: "it-IT",
         },
+
         'success': function (risposta) {
           $(".cds-container").empty();
           var source = $('#entry-template').html();
  	        var template = Handlebars.compile(source);
-          for(var i=0; i<risposta.results.length; i++){
-              var context= {
-                "titolo": risposta.results[i].title,
-                "titoloOriginale":risposta.results[i].original_title,
-                "lingua":risposta.results[i].original_language,
-                "voto":risposta.results[i].vote_average,
-              };
-              var html = template(context);
-              $(".cds-container").append(html);
+          if (risposta.total_results >= 1 ){
+            for(var i=0; i<risposta.results.length; i++){
+                var context= {
+                  "titolo": risposta.results[i].title,
+                  "titoloOriginale":risposta.results[i].original_title,
+                  "lingua":risposta.results[i].original_language,
+                  "voto":risposta.results[i].vote_average,
+                };
+                  var html = template(context);
+                  $(".cds-container").append(html);
+            }
+          }else {
+            $(".cds-container").html("film non è presente");
           }
 
         },
         'error': function () {
-          alert("E' avvenuto un errore. ");
+          alert("E' avvenuto un errore ");
         }
       }
     );
   })
 });
+
+
+// if ( data.query.includes(valore)) {
+//
+// } else {
+//   var source = $('#entry-template').html();
+//   var template = Handlebars.compile(source);
+//
+//   var html = "FILM NON PRESENTE";
+//   $(".cds-container").append(html);
+// }
