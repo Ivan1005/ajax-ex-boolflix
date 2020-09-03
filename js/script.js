4cd9f7bdcd9f1e9d8fbc8c08084043385492ac67
Ivan <matteraivam@gmail.com>
2020-09-03 09:51:01 +0200

    trasformato in funzione esterna

--- js/script.js
+++ js/script.js
@@ -1,51 +1,46 @@
 $(document).ready(function() {
   $("#but").click(function(){
     var valore = $("#in").val();
-    $.ajax(
-      {
-        'url': "https://api.themoviedb.org/3/search/movie",
-        'method': "GET",
-        "data" : {
-                    api_key: "0ddb3a09479589fbbf168ce8b9819f87",
-                    query: valore,
-                    language: "it-IT",
-        },
-
-        'success': function (risposta) {
-          $(".cds-container").empty();
-          var source = $('#entry-template').html();
- 	        var template = Handlebars.compile(source);
-          if (risposta.total_results >= 1 ){
-            for(var i=0; i<risposta.results.length; i++){
-                var context= {
-                  "titolo": risposta.results[i].title,
-                  "titoloOriginale":risposta.results[i].original_title,
-                  "lingua":risposta.results[i].original_language,
-                  "voto":risposta.results[i].vote_average,
-                };
-                  var html = template(context);
-                  $(".cds-container").append(html);
-            }
-          }else {
-            $(".cds-container").html("film non è presente");
-          }
-          $("#in").val("");
-        },
-        'error': function () {
-          alert("devi inserire il titolo");
-        }
-      }
-    );
+    film(valore);
   })
 });
 
 
-// if ( data.query.includes(valore)) {
-//
-// } else {
-//   var source = $('#entry-template').html();
-//   var template = Handlebars.compile(source);
-//
-//   var html = "FILM NON PRESENTE";
-//   $(".cds-container").append(html);
-// }
+function film (data){
+  $.ajax(
+    {
+      'url': "https://api.themoviedb.org/3/search/movie",
+      'method': "GET",
+      "data" : {
+                  api_key: "0ddb3a09479589fbbf168ce8b9819f87",
+                  query: data,
+                  language: "it-IT",
+      },
+
+      'success': function (risposta) {
+        $(".cds-container").empty();
+        var source = $('#entry-template').html();
+        var template = Handlebars.compile(source);
+        if (risposta.total_results >= 1 ){
+          for(var i=0; i<risposta.results.length; i++){
+              var context= {
+                "titolo": risposta.results[i].title,
+                "titoloOriginale":risposta.results[i].original_title,
+                "lingua":risposta.results[i].original_language,
+                "voto":risposta.results[i].vote_average,
+              };
+                var html = template(context);
+                $(".cds-container").append(html);
+          }
+        }else {
+          $(".cds-container").html("film non è presente");
+        }
+        $("#in").val("");
+      },
+      'error': function () {
+        alert("devi inserire il titolo");
+      }
+    }
+  );
+
+}
